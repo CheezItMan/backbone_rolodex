59d8513a3a973ebf560465c04433e78bbfa40081
Chris <chris@adadevelopersacademy.org>
2016-11-09 22:18:13 -0800

    API working.

--- app/js/collections/contacts.js
+++ app/js/collections/contacts.js
@@ -3,6 +3,9 @@
 ContactManager.Collections.Contacts = Backbone.Collection.extend({
   initialize: function() {
     this.listenTo(this.model, 'remove', this.remove);
+    this.comparator = function(model) {
+      return model.get("id");
+    }
   },
   model: ContactManager.Models.Contact,
   url: 'http://localhost:3000/contacts/'
